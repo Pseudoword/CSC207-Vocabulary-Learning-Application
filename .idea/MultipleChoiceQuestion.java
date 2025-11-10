be98e2b8def9c66fa6d35b6fe7044616baa61f56
lan-t-huynh <l.h.lanhuynh@gmail.com>
2025-11-10 18:17:15 -0500

    Added some methods for MultipleChoiceQuestion

--- .idea/MultipleChoiceQuestion.java
+++ .idea/MultipleChoiceQuestion.java
@@ -10,4 +10,35 @@ public class MultipleChoiceQuestion {
         this.choices = choices;
         this.answer = answer;
     }
+
+    public Vocabulary getWord() {
+        return word;
+    }
+
+    public List<String> getChoices() {
+        return choices;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean checkAnswer(String userChoice){
+        return answer.equals(userChoice);
+    }
+
+    public void displayQuestion() {
+        System.out.println("What is the definition of: " + word.getWord() + "?");
+        for (int i = 0; i < choices.size(); i++) {
+            System.out.println((i + 1) + ". " + choices.get(i)); //prints options as "1. choice" "2. choice" etc.
+        }
+    }
+
+    public int getCorrectChoiceIndex() {
+        return choices.indexOf(answer);
+    }
+
+    public int getChoiceIndex(String userChoice) {
+        return choices.indexOf(userChoice);
+    }
 }
