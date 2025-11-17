e8523451a45d0f6a97e995107efe8e6f285726d3
jinuotao <jinuotao@gmail.com>
2025-11-17 16:35:01 -0500

    In progress implementation of Update Deck Details use case with DAO and interactor classes

---
+++ src/main/java/use_case/update_deck_details/UpdateDeckDetailsDataAccessInterface.java
@@ -0,0 +1,15 @@
+package use_case.update_deck_details;
+
+import entity.Deck;
+
+/**
+ * The DAO interface for the Update Deck Details Use Case.
+ */
+public interface UpdateDeckDetailsDataAccessInterface {
+
+    /**
+     * Updates the system to record this deck's title and description.
+     * @param deck the deck whose title and description is to be updated
+     */
+    void updateDeckDetails(Deck deck);
+}
