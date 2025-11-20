8b7f3aa40b9c3e2fb25b2cac54db49463586721d
Nerissa-W <nerissa.wang@mail.utoronto.ca>
2025-11-20 18:12:23 -0500

    Add controller and presenter for Create Deck

--- src/main/java/interface_adapter/create_deck/CreateDeckController.java
+++ src/main/java/interface_adapter/create_deck/CreateDeckController.java
@@ -1,4 +1,19 @@
 package main.java.interface_adapter.create_deck;
 
+import main.java.use_case.create_deck.CreateDeckInputBoundary;
+import main.java.use_case.create_deck.CreateDeckInputData;
+
 public class CreateDeckController {
+
+    private final CreateDeckInputBoundary interactor;
+
+    public CreateDeckController(CreateDeckInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void createDeck(String title, String description) {
+        CreateDeckInputData inputData = new CreateDeckInputData(title, description);
+        interactor.execute(inputData);
+    }
 }
+
--- src/main/java/interface_adapter/create_deck/CreateDeckPresenter.java
+++ src/main/java/interface_adapter/create_deck/CreateDeckPresenter.java
@@ -1,4 +1,17 @@
 package main.java.interface_adapter.create_deck;
 
-public class CreateDeckPresenter {
+import main.java.use_case.create_deck.CreateDeckOutputBoundary;
+import main.java.use_case.create_deck.CreateDeckOutputData;
+
+public class CreateDeckPresenter implements CreateDeckOutputBoundary {
+
+    @Override
+    public void prepareSuccessView(CreateDeckOutputData outputData) {
+        System.out.println("Deck created: " + outputData.getTitle());
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        System.out.println("Failed to create deck: " + errorMessage);
+    }
 }
