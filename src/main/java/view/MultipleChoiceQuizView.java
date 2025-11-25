a6152d6edbb9af7e7adee45f379da2fa7e339ae3
lan-t-huynh <l.h.lanhuynh@gmail.com>
2025-11-25 14:03:35 -0500

    Convert to JPanel

--- src/main/java/view/MultipleChoiceQuizView.java
+++ src/main/java/view/MultipleChoiceQuizView.java
@@ -2,13 +2,14 @@ package view;
 
 import interface_adapter.multiple_choice_quiz.MultipleChoiceQuizController;
 import interface_adapter.multiple_choice_quiz.MultipleChoiceQuizViewModel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class MultipleChoiceQuizView extends JFrame {
+public class MultipleChoiceQuizView extends JPanel {
 
-    private final MultipleChoiceQuizController controller;
+    private MultipleChoiceQuizController controller;
     private final MultipleChoiceQuizViewModel viewModel;
 
     private JLabel wordLabel;
@@ -16,52 +17,53 @@ public class MultipleChoiceQuizView extends JFrame {
     private JLabel feedbackLabel;
     private JButton nextButton;
 
-    public MultipleChoiceQuizView(MultipleChoiceQuizController controller, MultipleChoiceQuizViewModel viewModel) {
-        this.controller = controller;
+    public MultipleChoiceQuizView(MultipleChoiceQuizViewModel viewModel) {
         this.viewModel = viewModel;
         setupUI();
         updateUI();
     }
 
+    public void setController(MultipleChoiceQuizController controller) {
+        this.controller = controller;
+    }
+
     private void setupUI() {
-        setTitle("Multiple Choice Quiz");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
         wordLabel = new JLabel("", SwingConstants.CENTER);
         wordLabel.setFont(new Font("Arial", Font.BOLD, 16));
         add(wordLabel, BorderLayout.NORTH);
 
-        JPanel choicesPanel = new JPanel();
-        choicesPanel.setLayout(new GridLayout(4, 1));
+        JPanel choicesPanel = new JPanel(new GridLayout(4, 1));
         choiceButtons = new JButton[4];
+
         for (int i = 0; i < 4; i++) {
             int index = i;
             choiceButtons[i] = new JButton();
             choiceButtons[i].addActionListener(e -> {
-                controller.answerSelected(index);
+                if (controller != null) controller.answerSelected(index);
                 updateUI();
             });
             choicesPanel.add(choiceButtons[i]);
         }
+
         add(choicesPanel, BorderLayout.CENTER);
 
         JPanel bottomPanel = new JPanel(new BorderLayout());
         feedbackLabel = new JLabel("", SwingConstants.CENTER);
         nextButton = new JButton("Next");
         nextButton.addActionListener((ActionEvent e) -> {
-            controller.nextPressed();
+            if (controller != null) controller.nextPressed();
             updateUI();
         });
 
         bottomPanel.add(feedbackLabel, BorderLayout.CENTER);
         bottomPanel.add(nextButton, BorderLayout.SOUTH);
-
         add(bottomPanel, BorderLayout.SOUTH);
     }
 
-    private void updateUI() {
+    public void updateUI() {
+        super.updateUI();
         wordLabel.setText(viewModel.getCurrentWord());
         if (viewModel.getChoices() != null) {
             for (int i = 0; i < viewModel.getChoices().size(); i++) {
@@ -78,5 +80,9 @@ public class MultipleChoiceQuizView extends JFrame {
         if (viewModel.isQuizFinished()) {
             wordLabel.setText("Quiz Finished!");
         }
+    }
+
+    public String getViewName() {
+        return "multiple_choice_quiz";
     }
 }
