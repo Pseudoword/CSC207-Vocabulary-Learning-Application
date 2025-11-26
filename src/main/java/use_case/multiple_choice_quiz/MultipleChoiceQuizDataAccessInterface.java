aac9d733d3b68b8dcaebe4cb7ebc6bd35affddb8
lan-t-huynh <l.h.lanhuynh@gmail.com>
2025-11-25 21:48:22 -0500

    These files were not used

--- src/main/java/use_case/multiple_choice_quiz/MultipleChoiceQuizDataAccessInterface.java
+++
@@ -1,8 +0,0 @@
-package use_case.multiple_choice_quiz;
-
-import entity.MultipleChoiceQuestion;
-import java.util.List;
-
-public interface MultipleChoiceQuizDataAccessInterface {
-    List<MultipleChoiceQuestion> getQuestionsForDeck(String deckTitle);
-}
